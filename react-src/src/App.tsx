291eb6d3eb7a96c5b76b750e7fe86dbe41ee9a88
JustPilz <dkrivyakov@gmail.com>
2025-12-31 01:33:05 +0300

    update neutralino to 6.4.0, up deps, show app arguments

--- react-src/src/App.tsx
+++ react-src/src/App.tsx
@@ -2,10 +2,10 @@ import * as S from './styled';
 
 import { version as reactVersion, useEffect, useState } from 'react';
 
-import { computer } from '@neutralinojs/lib';
+import { computer, type OSInfo } from '@neutralinojs/lib';
 
 export function App() {
-  const [osInfo, setOsInfo] = useState<computer.OSInfo>();
+  const [osInfo, setOsInfo] = useState<OSInfo>();
 
   useEffect(() => {
     async function getOSInfo() {
@@ -16,14 +16,18 @@ export function App() {
   }, []);
 
   return (
-    <S.Wrapper>
-      <S.H3>Neutralinojs + React + TS + Vite template</S.H3>
-      <S.Paragraph>
-        OS: {osInfo?.name} {osInfo?.version}
-      </S.Paragraph>
-      <S.Paragraph>Neu binaryVersion: {window.NL_VERSION}</S.Paragraph>
-      <S.Paragraph>Neu clientVersion: {window.NL_CVERSION}</S.Paragraph>
-      <S.Paragraph>React version: {reactVersion}</S.Paragraph>
-    </S.Wrapper>
+    <S.Container>
+      <S.Wrapper>
+        <S.H3>Neutralinojs + React + TS + Vite template</S.H3>
+        <S.Paragraph>
+          OS: {osInfo?.name} {osInfo?.version}
+        </S.Paragraph>
+        <S.Paragraph>Neu binaryVersion: {window.NL_VERSION}</S.Paragraph>
+        <S.Paragraph>Neu clientVersion: {window.NL_CVERSION}</S.Paragraph>
+        <S.Paragraph>React version: {reactVersion}</S.Paragraph>
+        <S.Paragraph>App arguments: </S.Paragraph>
+        <pre>{JSON.stringify(window.NL_ARGS, null, 2)}</pre>
+      </S.Wrapper>
+    </S.Container>
   );
 }
--- react-src/src/styled.ts
+++ react-src/src/styled.ts
@@ -1,11 +1,20 @@
 import styled from 'styled-components';
 
-export const Wrapper = styled.div`
+export const Container = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   width: 100vw;
+  height: 100vh;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 60%;
   height: 100vh;
 `;
 
